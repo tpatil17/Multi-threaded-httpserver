a17a7fc9e5d48ba15f794b728979276824109f53
tanishq <tmpatil@ucsc.edu>
2023-01-18 02:34:24 +0000

    clang format

--- asgn1/memory.c
+++ asgn1/memory.c
@@ -7,109 +7,109 @@
 
 void get(int fd) {
 
-  char buff[1024];
-  int red;
+    char buff[1024];
+    int red;
 
-  while ((red = read(fd, buff, 1023)) > 0) {
+    while ((red = read(fd, buff, 1023)) > 0) {
 
-    buff[red] = '\0';
+        buff[red] = '\0';
 
-    if ((write(STDOUT_FILENO, buff, red)) == -1) {
-      perror("write");
-      exit(1);
+        if ((write(STDOUT_FILENO, buff, red)) == -1) {
+            perror("write");
+            exit(1);
+        }
     }
-  }
 
-  return;
+    return;
 }
 
 int main(int argc, char *argv[]) {
 
-  if (strcmp(argv[1], "get") == 0) {
+    if (strcmp(argv[1], "get") == 0) {
 
-    if (argc < 3) {
-      printf("Invalid Command\n");
-      return 1;
+        if (argc < 3) {
+            printf("Invalid Command\n");
+            return 1;
+        }
+
+        if (argc >= 4) {
+            if (argc > 4) {
+                printf("Invalid Command\n");
+                return 1;
+            }
+
+            if (strcmp(argv[3], "n") != 0) {
+                printf("Invalid Command\n");
+                return 1;
+            }
+
+            else {
+
+                if (access(argv[2], F_OK) != 0) {
+                    printf("Invalid Command\n");
+                    return 1;
+                } else {
+
+                    int fd = open(argv[2], O_RDONLY);
+                    get(fd);
+                    close(fd);
+                    return 0;
+                }
+            }
+
+        }
+
+        else {
+
+            if (access(argv[2], F_OK) != 0) {
+                printf("Invalid Command\n");
+
+                return 1;
+            } else {
+
+                int fd = open(argv[2], O_RDONLY);
+                get(fd);
+                close(fd);
+                return 0;
+            }
+        }
     }
+    if (strcmp(argv[1], "set") == 0) {
 
-    if (argc >= 4) {
-      if (argc > 4) {
-        printf("Invalid Command\n");
-        return 1;
-      }
+        char buff[1024];
 
-      if (strcmp(argv[3], "n") != 0) {
-        printf("Invalid Command\n");
-        return 1;
-      }
-
-      else {
+        if (argc < 3) {
+            printf("Invalid Command\n");
+            return 1;
+        }
 
         if (access(argv[2], F_OK) != 0) {
-          printf("Invalid Command\n");
-          return 1;
-        } else {
+            printf("Invlaid Command\n");
 
-          int fd = open(argv[2], O_RDONLY);
-          get(fd);
-          close(fd);
-          return 0;
+            return 1;
         }
-      }
 
-    }
+        int fd = open(argv[2], O_WRONLY | O_TRUNC);
 
-    else {
+        int red;
 
-      if (access(argv[2], F_OK) != 0) {
-        printf("Invalid Command\n");
+        while ((red = read(0, buff, 1023)) > 0) {
 
-        return 1;
-      } else {
+            buff[red] = '\0';
 
-        int fd = open(argv[2], O_RDONLY);
-        get(fd);
+            if ((write(fd, buff, red)) == -1) {
+                perror("write");
+                exit(1);
+            }
+            // memset(buff, NULL, 1024);
+        }
+
         close(fd);
+
         return 0;
-      }
+    } else {
+
+        printf("Invalid Command\n");
+        return 1;
     }
-  }
-  if (strcmp(argv[1], "set") == 0) {
-
-    char buff[1024];
-
-    if (argc < 3) {
-      printf("Invalid Command\n");
-      return 1;
-    }
-
-    if (access(argv[2], F_OK) != 0) {
-      printf("Invlaid Command\n");
-
-      return 1;
-    }
-
-    int fd = open(argv[2], O_WRONLY | O_TRUNC);
-
-    int red;
-
-    while ((red = read(0, buff, 1023)) > 0) {
-
-      buff[red] = '\0';
-
-      if ((write(fd, buff, red)) == -1) {
-        perror("write");
-        exit(1);
-      }
-      // memset(buff, NULL, 1024);
-    }
-
-    close(fd);
-
-    return 0;
-  } else {
-
-    printf("Invalid Command\n");
-    return 1;
-  }
 }
