da8a47d67ebb06bdbcae6430e190acd2f9ff3ade
Tanishq Patil <tmpatil@ucsc.edu>
2023-01-20 22:55:56 -0800

    Replace memory.c

--- asgn1/memory.c
+++ asgn1/memory.c
@@ -27,19 +27,19 @@ int main() {
     char init[4096] = "";
 
     char *token;
-    //printf("try out\n");
+
 
     ret = strchr(read_buf, '\n');
 
     if (ret == NULL) {
 
-        //  write(STDERR_FILENO, "It's me strchr\n", strlen("It's me strchr\n"));
+        
         write(STDERR_FILENO, "Invalid Command\n", strlen("Invalid Command\n"));
         return 1;
     }
     strcpy(init, ret);
 
-    //printf("%s\n", ret); // ----i-- remove --
+    
 
     token = strtok(read_buf, "\n");
 
@@ -50,13 +50,11 @@ int main() {
 
     int val = is_file(file_name);
 
-    // printf("token: %s\n", token); // remove --------------
+
 
     if (val != 0) {
-        //printf("odly its me\n");
-        //printf("%s\n", file_name);
-        //	printf("%d\n", val);
-        //	printf("still a problem\n");
+       
+
         write(STDERR_FILENO, "Invalid Command\n", strlen("Invalid Command\n"));
         return 1;
     }
@@ -86,7 +84,7 @@ int main() {
     if (strcmp(function, "get") == 0) {
         if (red > offset + 1) {
             write(STDERR_FILENO, "Invalid Command\n", strlen("Invalid Command\n"));
-            //printf("more chars than exp red: %d, exp: %d\n", red, offset);
+     
             return 1;
         }
         int fd = open(file_name, O_RDONLY);
@@ -113,7 +111,6 @@ int main() {
     }
     if (strcmp(function, "set") == 0) {
 
-        //write(STDOUT_FILENO,"in set\n" ,strlen("in set\n"));
 
         int set_read = 0;
         char buff[4096] = "";
@@ -122,20 +119,14 @@ int main() {
 
         if (fd2 < 0) {
             write(STDERR_FILENO, "Invalid Command\n", strlen("Invalid Command\n"));
-            //printf("%d\n", fd2);
-            //perror( "Error: \n");
+
             return 1;
         }
 
-        //printf("token again: %s\n", token);
-
-        //	int off = 0;
-
-        //	off = strlen(token);
-
+     
         token = strtok(NULL, "");
 
-        //printf("Current token: %s\n", token);
+      
         if (token != NULL) {
 
             write(fd2, token, strlen(token));
@@ -150,36 +141,28 @@ int main() {
             new_init[i] = init[i + 1];
         }
 
-        //lseek(fd2, 1, SEEK_CUR);
-        //if (ret != NULL){
-        // printf("%lu\n", strlen(init)); // ---- remove
+    
 
         write(fd2, new_init, strlen(new_init));
-        //}
-        //printf("%s\n", token); /////// remove --------------------
-
-        //printf("token passed\n");
-        //
-
-        //lseek(fd2, 0, SEEK_CUR);
+      
 
         while ((set_read = read(STDIN_FILENO, buff, 4095)) > 0) {
 
             buff[set_read] = '\0';
 
-            //	    printf("%s", buff);
+
 
             write(fd2, buff, set_read);
         }
 
-        //printf("reached the end\n");
+     
 
         close(fd2);
         write(STDOUT_FILENO, "OK\n", strlen("OK\n"));
         return 0;
 
     } else {
-        //write(STDOUT_FILENO, "problem found\n", strlen("problem found\n"));
+
         write(STDERR_FILENO, "Invalid Command\n", strlen("Invalid Command\n"));
         return 1;
     }
