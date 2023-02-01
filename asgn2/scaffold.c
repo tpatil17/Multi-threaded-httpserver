e3218b3e9119d20b45820d892a9b05eb22910982
Tanishq Patil <tmpatil@ucsc.edu>
2023-02-01 12:51:54 -0800

    Replace scaffold.c

--- asgn2/scaffold.c
+++ asgn2/scaffold.c
@@ -19,14 +19,58 @@ uint16_t strtouint16(char number[]) {
   return num;
 }
 
+/*
+a struct to store the response
+*/
+
+struct Response {
+  char version[24] = "";
+  int status_code = 0;
+  char status_phrase[100] = "";
+  char header[19] = "";
+  long length = 0;
+  char message[64] = "";
+};
+
 void handle_connection(int connfd){
     char buffer[4096] = "";
 
+    char resp_buffer[4096] = "";
+
     int bytes_read = 0;
+
+    struct Response res;
     
     bytes_read = read(connfd, buffer, 4096);
 
-    write(STDOUT_FILENO, buffer, bytes_read);
+    if (write(STDOUT_FILENO, buffer, bytes_read) < 0){
+        res.status_code = 400;
+        strcpy(res.version, "HTTP/1.1");
+        strcpy(res.status_phrase, "Bad Request");
+        res.length = strlen("Bad Request\n");
+        strcpy(res.header, "Content-Length");
+        strcpy(res.message, "Bad Request\n");
+        sprintf(resp_buf, "%s %d %s\r\n%s: %ld\r\n\r\n%s", res.version,
+            res.status_code, res.status_phrase, res.header, res.length,
+            res.message);
+        write(connfd, resp_buf, strlen(resp_buf));
+
+        strcpy(resp_buf, "");
+
+    }
+        res.status_code = 200;
+        strcpy(res.version, "HTTP/1.1");
+        strcpy(res.status_phrase, "OK");
+        res.length = strlen("OK\n");
+        strcpy(res.header, "Content-Length");
+        strcpy(res.message, "OK\n");
+        sprintf(resp_buf, "%s %d %s\r\n%s: %ld\r\n\r\n%s", res.version,
+            res.status_code, res.status_phrase, res.header, res.length,
+            res.message);
+        write(connfd, resp_buf, strlen(resp_buf));
+
+        strcpy(resp_buf, "");
+
 
     return;
 
@@ -49,16 +93,16 @@ int main(int argc, char *argv[]){
         errx(1, "Invalid Port\n");
     }
 
-    Listener_Socket *sock = NULL;
+    Listener_Socket sock;
 
-    listen_val = listener_init(sock, port);
+    listen_val = listener_init(&sock, port);
 
     if(listen_val != 0){
         errx(1, "Invalid Port\n");
     }
 
     while(1){
-        int connfd = listener_accept(sock);
+        int connfd = listener_accept(&sock);
         if(connfd < 0){
             warn("Accept Error");
             continue;
