9b1b47693ed155590bb425d5df46ef1b292d7660
Tanishq Patil <tmpatil@ucsc.edu>
2023-02-06 14:43:48 -0800

    Replace scaffold.c

--- asgn2/scaffold.c
+++ asgn2/scaffold.c
@@ -32,6 +32,288 @@ struct Response {
   char message[64];
 };
 
+struct Request
+{
+    char method[10];
+    char uri[64];
+    char version[25];
+    char header[50];
+    char value[50];
+    int off_set;
+    int err_flag; // 1: Bad Request, 2: Internal Server Error, 3: File not Found
+    int length;
+
+};
+
+struct Request process_request(char req_buffer[]){
+
+    struct Request req;
+
+    char p_buf[1024] = "";
+
+    strcpy(req.method, "");
+    strcpy(req.uri, "");
+    strcpy(req.version, "");
+    strcpy(req.header, "");
+    strcpy(req.value, "");
+    
+    struct Response res;
+
+  const char delim[2] = "\n";
+
+  char *token;
+
+  int total = 0;
+
+  char perm_header[25] = "";
+
+  char perm_val[24] = "";
+
+  int check = 0;
+
+  req.err_flag = 0;
+
+  int ctr = 0;
+
+  req.size = strlen(req_buffer);
+
+  token = strtok(req_buffer, delim);
+
+  strcpy(p_buf, token);
+
+  if ((check = sscanf(p_buf, "%s /%s %s %n", req.method, req.uri, req.version,
+                      &req.off_set)) != 3) {
+
+    
+    strcpy(req.method, "");
+    strcpy(req.version, "");
+    strcpy(req.uri, "");
+
+    sscanf(p_buf, "%s %s %s", req.method, req.uri, req.version);
+
+    if (strcmp(req.uri, "/") == 0 && strcmp(req.version, "HTTP/1.1") == 0) {
+
+    //   strcpy(res.version, "HTTP/1.1");
+    //   res.status_code = 500;
+    //   strcpy(res.status_phrase, "Internal Server Error");
+    //   strcpy(res.header, "Content-Length");
+    //   strcpy(res.message, "Internal Server Error\n");
+    //   res.length = strlen(res.message);
+    //   sprintf(buffer, "%s %d %s\r\n%s: %ld\r\n\r\n%s", res.version,
+    //           res.status_code, res.status_phrase, res.header, res.length,
+    //           res.message);
+    //   write(connfd, buffer, strlen(buffer));
+
+      req.err_flag = 2;
+
+      strcpy(p_buf "");
+      return req;
+    }
+
+    // strcpy(res.version, "HTTP/1.1");
+    // res.status_code = 400;
+    // strcpy(res.status_phrase, "Bad Request");
+    // strcpy(res.header, "Content-Length");
+    // res.length = 12;
+    // sprintf(buffer, "%s %d %s\r\n%s: %ld\r\n\r\n", res.version, res.status_code,
+    //         res.status_phrase, res.header, res.length);
+    // write(connfd, buffer, strlen(buffer));
+    // write(connfd, "Bad Request\n", 12);
+
+    req.err_flag = 1;
+
+    strcpy(p_buf, "");
+    return req;
+  }
+
+  token = strtok(NULL, delim);
+
+  total = req.off_set;
+
+  while (token != NULL) {
+
+    strcpy(p_buf, token);
+
+    sscanf(p_buf, "%s %s %n", req.header, req.value, &req.off_set);
+
+    char temp_1[25] = "";
+    char temp_2[25] = "";
+    char temp_3[25] = "";
+
+    if (sscanf(p_buf, "%s %s %s", temp_1, temp_2, temp_3) == 3) {
+
+    //   strcpy(res.version, "HTTP/1.1");
+    //   res.status_code = 400;
+    //   strcpy(res.status_phrase, "Bad Request");
+    //   strcpy(res.header, "Content-Length");
+    //   res.length = 12;
+    //   sprintf(buffer, "%s %d %s\r\n%s: %ld\r\n\r\n", res.version,
+    //           res.status_code, res.status_phrase, res.header, res.length);
+    //   write(connfd, buffer, strlen(buffer));
+    //   write(connfd, "Bad Request\n", 12);
+
+      req.err_flag = 1;
+
+      strcpy(p_buf, "");
+      strcpy(temp_1, "");
+      strcpy(temp_2, "");
+      strcpy(temp_3, "");
+
+      return req;
+    }
+
+    if (strcmp(req.header, "") == 0 && strcmp(req.value, "") == 0) {
+
+      if (ctr == 0 &&
+          (strcmp(req.method, "GET") != 0 | strcmp(req.method, "get") != 0)) {
+
+        // strcpy(res.version, "HTTP/1.1");
+        // res.status_code = 400;
+        // strcpy(res.status_phrase, "Bad Request");
+        // strcpy(res.header, "Content-Length");
+        // res.length = 12;
+        // sprintf(buffer, "%s %d %s\r\n%s: %ld\r\n\r\n", res.version,
+        //         res.status_code, res.status_phrase, res.header, res.length);
+        // write(connfd, buffer, strlen(buffer));
+        // write(connfd, "Bad Request\n", 12);
+
+        req.err_flag = 1;
+
+        strcpy(p_buf, "");
+        return req;
+      }
+
+      break;
+    }
+
+    if (strcmp(req.header, "Content-Length:") == 0) {
+
+      strcpy(perm_header, req.header);
+
+      char temp1[25] = "";
+      char temp2[25] = "";
+      char temp3[25] = "";
+
+      int bad_flag = 0;
+
+      if (sscanf(p_buf, "%s %s %s", temp1, temp2, temp3) == 3) {
+
+        bad_flag = 1;
+
+        strcpy(temp1, "");
+        strcpy(temp2, "");
+        strcpy(temp3, "");
+      }
+
+      int val_ln = 0;
+
+      val_ln = strlen(req.value);
+
+      int i;
+
+      for (i = 0; i < val_ln; i++) {
+
+        if (!isdigit(req.value[i])) {
+
+          bad_flag = 1;
+          break;
+        }
+      }
+
+      if (strcmp(req.value, "") == 0 | bad_flag == 1) {
+        // strcpy(res.version, "HTTP/1.1");
+        // res.status_code = 400;
+        // strcpy(res.status_phrase, "Bad Request");
+        // strcpy(res.header, "Content-Length");
+        // res.length = 12;
+        // sprintf(buffer, "%s %d %s\r\n%s: %ld\r\n\r\n", res.version,
+        //         res.status_code, res.status_phrase, res.header, res.length);
+        // write(connfd, buffer, strlen(buffer));
+        // write(connfd, "Bad Request\n", 12);
+
+        req.err_flag = 1;
+
+        strcpy(p_buf, "");
+        return req; // bad request
+      }
+
+      strcpy(perm_val, req.value);
+    }
+
+    if (strcmp(req.header, "") == 0) {
+
+    //   strcpy(res.version, "HTTP/1.1");
+    //   res.status_code = 400;
+    //   strcpy(res.status_phrase, "Bad Request");
+    //   strcpy(res.header, "Content-Length");
+    //   res.length = 12;
+    //   sprintf(buffer, "%s %d %s\r\n%s: %ld\r\n\r\n", res.version,
+    //           res.status_code, res.status_phrase, res.header, res.length);
+    //   write(connfd, buffer, strlen(buffer));
+    //   write(connfd, "Bad Request\n", 12);
+
+      req.err_flag = 1;
+
+      strcpy(p_buf, "");
+
+      return req;
+    }
+
+    strcpy(req.header, "");
+
+    strcpy(req.value, "");
+
+    token = strtok(NULL, delim);
+
+    total += req.off_set;
+
+    ctr += 1;
+  }
+
+  strcpy(req.header, perm_header);
+
+  strcpy(req.value, perm_val);
+
+  req.off_set = total + 8;
+
+  if (ctr == 1) {
+
+    req.off_set -= 4;
+  }
+
+  req.length = atoi(perm_val);
+
+  return req;
+}
+
+
+
+
+void Get(char file[], int connfd){
+
+    int fd = 0;
+
+    fd = open(file);
+
+    if(fd < 0){
+        errx(EXIT_FAILURE, "File did not open succesfully");
+    }
+    int read_file = 0;
+
+    char file_content[4096] = "";
+
+    while((read_file = read(fd, file_content, 4095)) > 0){
+
+        if (write(connfd, file_content, read_file) < 0){
+            errx(EXIT_FAILURE, "write error");
+        }
+
+        strcpy(file_content, ""); //reset buffer to empty state 
+    }
+    close(fd);
+    return;
+}
+
 void handle_connection(int connfd){
     char buffer[4096] = "";
 
@@ -42,6 +324,19 @@ void handle_connection(int connfd){
     struct Response res;
     
     bytes_read = read(connfd, buffer, 4096);
+
+    struct Request req;
+
+    req = process_request(buffer);
+
+    if (strcmp(req.method, "GET") == 0| strcmp(req.method, "get")){
+        printf("request processed succesfully, implement get\n");
+
+    }
+    if (strcmp(req.method, "PUT") == 0 | strcmp(req.method, "put") == 0){
+        printf("request processed succesfully, implement put");
+    }
+
 
     if (write(STDOUT_FILENO, buffer, bytes_read) < 0){
         res.status_code = 400;
