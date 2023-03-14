313e9fcdb689d134ea6b2469f9062530e88a63de
Tanishq Patil <tmpatil@ucsc.edu>
2023-03-14 14:47:41 -0700

    Replace httpserver.c

--- asgn4/httpserver.c
+++ asgn4/httpserver.c
@@ -80,7 +80,8 @@ int main(int argc, char **argv) {
     while (1) {
         int connfd;
         connfd = listener_accept(&sock);
-        void *ptr = connfd;
+        void *ptr;
+        ptr  = connfd;
         queue_push(task_queue, ptr); // Push the task to queue
         //handle_connection(connfd);
         //close(connfd);
