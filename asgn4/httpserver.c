2ff03c88a628d490e9f08c00353869e6376ca527
Tanishq Patil <tmpatil@ucsc.edu>
2023-03-14 20:20:35 -0700

    Replace httpserver.c

--- asgn4/httpserver.c
+++ asgn4/httpserver.c
@@ -125,7 +125,7 @@ void *worker_threads(){
         fprintf(stderr, "thread  is in\n");
         uintptr_t conn;
         queue_pop(task_queue,(void **)&conn);
-        fprintf(stderr, "conn: %lu", conn);
+        fprintf(stderr, "conn: %p", &conn);
         fprintf(stderr, "queue pop is smooth\n");
         handle_connection(conn);
         close(conn);
